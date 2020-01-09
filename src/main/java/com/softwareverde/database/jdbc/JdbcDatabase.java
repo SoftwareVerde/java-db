f3afaa8172677b193774ccafd51283edc8918d5c
Andrew Groot <groot@softwareverde.com>
2020-01-09 17:12:55 -0500

    Added convenience interface JdbcDatabase.

---
+++ src/main/java/com/softwareverde/database/jdbc/JdbcDatabase.java
@@ -0,0 +1,10 @@
+package com.softwareverde.database.jdbc;
+
+import com.softwareverde.database.Database;
+
+import java.sql.Connection;
+
+/**
+ * Convenience interface to avoid specifying the generic type everywhere when using JDBC.
+ */
+public interface JdbcDatabase extends Database<Connection> { }
